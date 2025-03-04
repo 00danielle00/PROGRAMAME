86fac3c7407c282242eb29f8e89841ee19a0d582
Danielle Bemfica <danielleludovicoo@gmail.com>
2025-03-04 18:10:51 +0100

    commit

--- ProblemasPROGRAMAME/src/main/java/org/example/Problema1.java
+++ ProblemasPROGRAMAME/src/main/java/org/example/Problema1.java
@@ -1,4 +1,23 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Problema1 {
+    static java.util.Scanner teclado;
+
+    public static void casoDePrueba() {
+        String soy = teclado.next();
+        String nombre = teclado.next();
+
+        System.out.println("Hola, "+ nombre+".");
+    }
+
+    public static void main(String[] args) {
+
+        teclado = new java.util.Scanner(System.in);
+        int numCasos = teclado.nextInt();
+        for (int i = 0; i < numCasos; i++) {
+            casoDePrueba();
+        }
+    }
 }
---
+++ ProblemasPROGRAMAME/src/main/java/org/example/Problema2.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Problema2 {
+    static Scanner in;
+
+    public static boolean casoDePrueba() {
+        String punto = "";
+        if (punto.equals("."))
+        return false;
+        else {
+            
+            return true;
+        }
+    }
+
+    public static void main(String[] args) {
+        in = new Scanner(System.in);
+        while (casoDePrueba()) {
+        }
+    }
+}
